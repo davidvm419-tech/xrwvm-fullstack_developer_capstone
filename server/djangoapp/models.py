fcd1183a99fa64da195481cc6b96ec746970a658
davidvm419-tech <davidvm419@outlook.com>
2026-05-09 14:01:21 -0400

    Added Car Make model.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,10 +8,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
