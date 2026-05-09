8509e81082f84f2ebdfc0fc1175a60daa9ab0c24
davidvm419-tech <davidvm419@outlook.com>
2026-05-09 11:57:11 -0400

    Aded login and logout functionality.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -39,13 +39,27 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""} 
+    return JsonResponse(data)
+
+
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    # Check method
+    if request.method == "POST":
+        # Get Json data
+        data = json.loads(request.body)
+        user_name = data["userName"]
+        password = data["password"]
+        first_name = data["firstName"]
+        last_name = data["lastName"]
+        email = data["email"]
+    
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
